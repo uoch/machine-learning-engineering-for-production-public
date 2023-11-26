b86e5c75a4c31668058cea00203a488b2508d5c0
uoch <chaouch.thameurtc61@gmail.com>
2023-11-26 08:06:35 +0100

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -9,14 +9,14 @@ from pydantic import BaseModel, conlist
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
-with open("models/wine.pkl", "rb") as file:
+with open("models/wine-95.pkl", "rb") as file:
     clf = pickle.load(file)
 
 
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_length=13, max_length=13)]
 
-
+#lanch the app with uvicorn
 @app.post("/predict")
 def predict(wine: Wine):
     batches = wine.batches
